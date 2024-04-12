bf8ae5209702222b32e34523eb35fd92d5f1324c
Garrett Downs <garredow@gmail.com>
2024-04-12 18:51:22 -0400

    fix type 2

---
+++ src/enums/MessageType.ts
@@ -0,0 +1,13 @@
+export enum MessageType {
+	RefreshElements = 'refresh-elements',
+	RefreshDeviceInfo = 'refresh-device-info',
+	RefreshStorage = 'refresh-storage',
+
+	DeviceInfoUpdate = 'device-info-update',
+	NewLog = 'new-log',
+	ClearLogs = 'clear-logs',
+	ElementsUpdate = 'elements-update',
+	StorageUpdate = 'storage-update',
+
+	Error = 'error'
+}
